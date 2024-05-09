0a17956b14aa3a670314fe83e660e57b388e3e17
ibuki2003 <main@fuwa.dev>
2024-05-09 11:54:03 +0900

    update settings

---
+++ README.md
@@ -0,0 +1,8 @@
+# IS CG demos with WebGL and wasm
+
+https://fuwa.dev/is_cg_gl/
+
+main source code files are in `wasm/[project]/src/`
+
+- [(m0_demo)](wasm/m0_demo/src)
+- [m1_spline](wasm/m1_spline/src)
--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,28 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 import { globSync } from "glob";
 
+const base = "/is_cg_gl";
+
 export default defineConfig({
+  base,
   root: __dirname,
+  plugins: [
+    htmlPlugin(),
+  ],
   build: {
     outDir: resolve(__dirname, "dist"),
     rollupOptions: {
-      input: Object.fromEntries(
-        globSync(resolve(__dirname, "*/index.html"))
-          .map((file) => {
-            const name = file.match(/(.*)\/index.html/)![1];
+      input: {
+        main: resolve(__dirname, "index.html"),
+        ...Object.fromEntries(
+          globSync(resolve(__dirname, "*/index.html"))
+            .map((file) => {
+              const name = file.match(/(\w*)\/index.html/)![1];
 
-            return [name, resolve(__dirname, `${name}/index.html`)];
-          })
-      ),
+              return [name, resolve(__dirname, `${name}/index.html`)];
+            })
+        ),
+      },
     },
   },
   resolve: {
@@ -24,4 +33,18 @@ export default defineConfig({
   },
 
 });
+
+function htmlPlugin() {
+  return {
+    name: "html-transform",
+    transformIndexHtml(html) {
+      return html.replace(/<a href="(.*)">/g, (match, p1) => {
+        if (p1.startsWith("/")) {
+          return match.replace(p1, `${base}${p1}`);
+        }
+        return match;
+      });
+    },
+  };
+}
 
